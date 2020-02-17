d28ba75ee26662ff3d02e223aa3cdadc35e4cf60
Thomas Wright <twwright@gmail.com>
2020-02-17 00:26:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/team' do
+    @name = params["name"]
+    @c = params["c"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @coach = params["coach"]
+    
+    erb :team
+  end
 
 end
